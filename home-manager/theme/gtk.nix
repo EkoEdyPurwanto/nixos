59db9c6472c65f12a1a50461bb7139e03892dfe0
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-05-02 20:20:28 +0700

    style: formating code

--- home-manager/theme/gtk.nix
+++ home-manager/theme/gtk.nix
@@ -3,28 +3,28 @@
 let
   name = "eep";
 in {
-  home-manager = {
-    users.${name} = {
-      gtk = {
-        enable = true;
-        font = {
-          name = "Iosevka NF SemiBold";
-          size= 11;
+    home-manager = {
+        users.${name} = {
+            gtk = {
+                enable = true;
+                font = {
+                name = "Iosevka NF SemiBold";
+                size= 11;
+                };
+                theme = {
+                name = "WhiteSur-Dark"; # WhiteSur-Dark-solid, WhiteSur-Light, WhiteSur-Light-solid
+                package = pkgs.whitesur-gtk-theme;
+                };
+                iconTheme = {
+                name = "candy-icons";
+                package = pkgs.candy-icons;
+                };
+                cursorTheme = {
+                name = "Bibata-Modern-Ice";
+                package = pkgs.bibata-cursors;
+                size = 18;
+                };
+            };
         };
-        theme = {
-          name = "WhiteSur-Dark"; # WhiteSur-Dark-solid, WhiteSur-Light, WhiteSur-Light-solid
-          package = pkgs.whitesur-gtk-theme;
-        };
-        iconTheme = {
-          name = "candy-icons";
-          package = pkgs.candy-icons;
-        };
-        cursorTheme = {
-          name = "Bibata-Modern-Ice";
-          package = pkgs.bibata-cursors;
-          size = 18;
-        };
-      };
     };
-  };
 }
