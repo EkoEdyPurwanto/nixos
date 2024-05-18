6ec250dd4df577b0ec720fdd32f5c08db762c33f
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-05-18 17:08:57 +0700

    feat: user settings of vscode

--- home-manager/program/vscode.nix
+++ home-manager/program/vscode.nix
@@ -11,10 +11,25 @@ in {
                     enableUpdateCheck = true;
                     enableExtensionUpdateCheck = true;
                     package = pkgs.vscode;
-                    extensions = [
-#                    pkgs.vscode-extensions.bbenoist.nix
+                    userSettings = {
+                        "files.autoSave" = "afterDelay";
+                        "workbench.iconTheme" = "vscode-icons";
+                        "workbench.colorTheme" = "Default High Contrast";
+                        "workbench.productIconTheme" = "emoji-product-icons";
+                        "editor.fontLigatures" = true;
+                        "editor.fontVariations" = false;
+                        "editor.fontWeight" = 625;
+                        "editor.fontFamily" = "Iosevka Nerd Font";
+                        "editor.fontSize" = 16;
+                    };
+                    extensions = with pkgs; [
+                        vscode-extensions.golang.go
+                        vscode-extensions.bbenoist.nix
+                        vscode-extensions.esbenp.prettier-vscode
+                        vscode-extensions.oderwat.indent-rainbow
+                        vscode-extensions.vscode-icons-team.vscode-icons
                     ];
-               };
+                };
             };
         };
     };
