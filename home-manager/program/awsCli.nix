2afe8da4ee7f9d4372b8506e4ba81a66eadf068d
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-05-26 00:51:47 +0700

    feat: add awscli

---
+++ home-manager/program/awsCli.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+let
+    name = "eep";
+in {
+    home-manager = {
+        users.${name} = {
+            programs = {
+                awscli = {
+                    enable = true;
+                    package = pkgs.awscli2;
+                    # settings = {};
+                    # credentials = {};
+            };
+        };
+    };
+}
