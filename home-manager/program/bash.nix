9764a0321d853821a4043b567de256bfc713984d
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-06-18 07:11:18 +0700

    chore: add fastfetch when open the terminal

--- home-manager/program/bash.nix
+++ home-manager/program/bash.nix
@@ -24,6 +24,7 @@ in {
                         #### BAT ###
                         cat = "bat";
                     };
+                    bashrcExtra = "fastfetch";
                 };
             };
         };
