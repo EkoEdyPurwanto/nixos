d592ce389daeca2f0823a562f26a6a37c60c9704
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-07-12 07:30:21 +0700

    feat: mysql db

---
+++ nixos/service/mySql.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+let
+    statsConfig = {
+        db = "myDatabase";
+        user = "eep";
+        password = "1903";
+    };
+
+in {
+    services.mysql = {
+        enable = true;
+        package = pkgs.mariadb;
+        ensureDatabases = [
+            statsConfig.db
+        ];
+        replication.role = "master";
+        replication.slaveHost = "127.0.0.1";
+        replication.masterUser = "${statsConfig.user}";
+        replication.masterPassword = "${statsConfig.password}";
+        initialScript = pkgs.writeText "setup-sql" ''
+            CREATE DATABASE IF NOT EXISTS `${statsConfig.db}`;
+            CREATE USER IF NOT EXISTS '${statsConfig.user}'@'localhost' IDENTIFIED WITH mysql_native_password;
+            ALTER USER '${statsConfig.user}'@'localhost' IDENTIFIED BY '${statsConfig.password}';
+            GRANT ALL PRIVILEGES ON *.* TO '${statsConfig.user}'@'localhost';
+        '';
+
+    };
+}
