fb14882ffb842fb75e2855f5c00fbbfa6a11c6fd
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-07-12 14:01:14 +0700

    fix: conf of mysql

--- nixos/service/mySql.nix
+++ nixos/service/mySql.nix
@@ -2,7 +2,7 @@
 
 let
     statsConfig = {
-        db = "myDatabase";
+        db = "eep";
         user = "eep";
         password = "1903";
     };
@@ -11,19 +11,15 @@ in {
     services.mysql = {
         enable = true;
         package = pkgs.mariadb;
-        ensureDatabases = [
-            statsConfig.db
-        ];
         replication.role = "master";
         replication.slaveHost = "127.0.0.1";
         replication.masterUser = "${statsConfig.user}";
         replication.masterPassword = "${statsConfig.password}";
         initialScript = pkgs.writeText "setup-sql" ''
             CREATE DATABASE IF NOT EXISTS `${statsConfig.db}`;
-            CREATE USER IF NOT EXISTS '${statsConfig.user}'@'localhost' IDENTIFIED WITH mysql_native_password;
-            ALTER USER '${statsConfig.user}'@'localhost' IDENTIFIED BY '${statsConfig.password}';
-            GRANT ALL PRIVILEGES ON *.* TO '${statsConfig.user}'@'localhost';
+            CREATE USER IF NOT EXISTS '${statsConfig.user}'@'%' IDENTIFIED WITH mysql_native_password;
+            ALTER USER '${statsConfig.user}'@'%' IDENTIFIED BY '${statsConfig.password}';
+            GRANT ALL PRIVILEGES ON * . * TO '${statsConfig.user}'@'%';
         '';
-
     };
 }
