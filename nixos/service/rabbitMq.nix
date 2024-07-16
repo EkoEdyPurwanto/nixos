9c124f023168985465978c34ef57be80bba700c9
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-07-16 14:53:39 +0700

    feat: rabbitmq conf

--- nixos/service/rabbitMq.nix
+++ nixos/service/rabbitMq.nix
@@ -1,26 +1,31 @@
 { pkgs, ... }:
 
 {
-    # services.epmd.package = pkgs.erlang;
-    services = {
-        rabbitmq = {
+    # rabbitmq
+    services.rabbitmq = {
+        enable = true;
+        package = pkgs.rabbitmq-server;
+        dataDir = "/var/lib/rabbitmq";
+        port = 5672;
+        listenAddress = "0.0.0.0";
+        # unsafeCookie = "";
+        plugins = [ 
+            "rabbitmq_management"
+            "rabbitmq_mqtt"
+        ];
+
+        managementPlugin = {
             enable = true;
-            package = pkgs.rabbitmq-server;
-            dataDir = "/var/lib/rabbitmq";
-            port = 5672;
-            listenAddress = "0.0.0.0";
-            # cookie = "";
-            managementPlugin.enable = true;
-            managementPlugin.port = 15672;
-            plugins = [
-                "rabbitmq_management"
-            ];
-            # pluginDirs = [
-                #
-            # ];
+            port = 15672;
         };
-        # onlyoffice = {
-        #     rabbitmqUrl = "amqp://eep:1903@localhost:5672";
-        # };
+    };
+
+    #
+    services.onlyoffice.rabbitmqUrl = "amqp://eep:1903@localhost:5672";
+
+    # erlang
+    services.epmd = {
+        enable = true;
+        package = pkgs.erlang;
     };
 }
