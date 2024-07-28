261be4299edd649e8c34d8f91372d49dabceebeb
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-07-28 17:07:00 +0700

    style: make code readeable

--- home-manager/theme/gtk.nix
+++ home-manager/theme/gtk.nix
@@ -7,22 +7,30 @@ in {
         users.${name} = {
             gtk = {
                 enable = true;
+
+                # FONT
                 font = {
-                name = "Iosevka NF SemiBold";
-                size= 11;
+                    name = "Iosevka NF SemiBold";
+                    size = 11;
                 };
+
+                # THEME
                 theme = {
-                name = "WhiteSur-Dark"; # WhiteSur-Dark-solid, WhiteSur-Light, WhiteSur-Light-solid
-                package = pkgs.whitesur-gtk-theme;
+                    name    = "WhiteSur-Dark"; # WhiteSur-Dark-solid, WhiteSur-Light, WhiteSur-Light-solid
+                    package = pkgs.whitesur-gtk-theme;
                 };
+
+                # ICON THEME
                 iconTheme = {
-                name = "candy-icons";
-                package = pkgs.candy-icons;
+                    name    = "candy-icons";
+                    package = pkgs.candy-icons;
                 };
+
+                # CURSOR THEME
                 cursorTheme = {
-                name = "Bibata-Modern-Ice";
-                package = pkgs.bibata-cursors;
-                size = 18;
+                    name    = "Bibata-Modern-Ice";
+                    package = pkgs.bibata-cursors;
+                    size    = 18;
                 };
             };
         };
