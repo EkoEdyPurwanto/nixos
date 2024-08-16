6d5ea99120096c6555762dcefd990363c560bbf4
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-08-16 21:56:25 +0700

    comment: extension

--- home-manager/program/chromium.nix
+++ home-manager/program/chromium.nix
@@ -8,16 +8,16 @@ in {
             programs = {
                 chromium = {
                     enable = true;
-                    extensions = [
-                        "bhchdcejhohfmigjafbampogmaanbfkg" # User-Agent Switcher and Manager
-                        "hnmpcagpplmpfojmgmnngilcnanddlhb" # Windscribe - Free Proxy and Ad Blocker
-                        "ohcpnigalekghcmgcdcenkpelffpdolg" # ColorPick Eyedropper
-                        "egjidjbpglichdcondbcbdnbeeppgdph" # Trust Wallet
-                        "bnjjngeaknajbdcgpfkgnonkmififhfo" # Fake Filler
-                        "cppjkneekbjaeellbfkmgnhonkkjfpdn" # Clear Cache
-                        "fdpohaocaechififmbbbbbknoalclacl" # GoFullPage - Full Page Screen Capture
-                        "kgkhambjbahgejgoaefmekdchedkihln" # Edit Anything
-                    ];
+                    # extensions = [
+                    #     "bhchdcejhohfmigjafbampogmaanbfkg" # User-Agent Switcher and Manager
+                    #     "hnmpcagpplmpfojmgmnngilcnanddlhb" # Windscribe - Free Proxy and Ad Blocker
+                    #     "ohcpnigalekghcmgcdcenkpelffpdolg" # ColorPick Eyedropper
+                    #     "egjidjbpglichdcondbcbdnbeeppgdph" # Trust Wallet
+                    #     "bnjjngeaknajbdcgpfkgnonkmififhfo" # Fake Filler
+                    #     "cppjkneekbjaeellbfkmgnhonkkjfpdn" # Clear Cache
+                    #     "fdpohaocaechififmbbbbbknoalclacl" # GoFullPage - Full Page Screen Capture
+                    #     "kgkhambjbahgejgoaefmekdchedkihln" # Edit Anything
+                    # ];
                 };
             };
         };
