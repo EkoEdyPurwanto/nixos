6e1a38bb1191fed3b259f4140c059472c0d12545
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-09-08 23:47:41 +0700

    feat: cointop

--- home-manager/package/package.nix
+++ home-manager/package/package.nix
@@ -27,6 +27,7 @@ in {
                     cmatrix         # matrix like hengker pro Tzy
                     cbonsai         # bonsai ASCII art
                     tty-clock       # to display the current time on terminal
+                    # cointop         # cryptocurrency tracking like htop
                 ];
             };
         };
