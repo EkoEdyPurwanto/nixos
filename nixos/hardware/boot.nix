a9138fe1a1573b99ed298da729288c4770397e7e
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-09-08 08:19:42 +0700

    chore: boot and fileSystem

--- nixos/hardware/boot.nix
+++ nixos/hardware/boot.nix
@@ -11,7 +11,7 @@
                 "rtsx_pci_sdmmc"
             ];
             kernelModules = [ ];
-            luks.devices."luks-8c05a8bd-c9eb-49d9-84f2-d1ce6c722f08".device = "/dev/disk/by-uuid/8c05a8bd-c9eb-49d9-84f2-d1ce6c722f08";
+            luks.devices. "luks-1e864ff3-d19e-491b-a401-78f9962dae85".device = "/dev/disk/by-uuid/1e864ff3-d19e-491b-a401-78f9962dae85";
         };
             kernelModules = [
                 "kvm-intel"
--- nixos/hardware/fileSystem.nix
+++ nixos/hardware/fileSystem.nix
@@ -2,16 +2,16 @@
 
 {
     fileSystems."/" = {
-        device = "/dev/disk/by-uuid/d6334ec0-fe06-4136-b8c1-e187f863fd95";
+        device = "/dev/disk/by-uuid/f49fba0e-7fe7-48e8-b413-0e2e3e229547";
         fsType = "ext4";
     };
 
     fileSystems."/boot" = {
-        device = "/dev/disk/by-uuid/E164-7B8D";
+        device = "/dev/disk/by-uuid/ABEC-C57F";
         fsType = "vfat";
-        options = [
-        "fmask=0022"
-        "dmask=0022"
+        options = [ 
+            "fmask=0077"
+            "dmask=0077" 
         ];
     };
 }
