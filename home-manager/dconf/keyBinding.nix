d64bf6d236906d3fd065783a53508e60c6a9fa4c
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-09-08 08:45:29 +0700

    chore: updates keyBind

--- home-manager/dconf/keyBinding.nix
+++ home-manager/dconf/keyBinding.nix
@@ -23,14 +23,18 @@ in {
                             "/${basePath}/custom3/"
                             "/${basePath}/custom4/"
                             "/${basePath}/custom5/"
+                            "/${basePath}/custom6/"
+                            "/${basePath}/custom7/"
                         ];
                     };
                     "${basePath}/custom0" = customKeybinding "<Super>t" "kitty" "open-kitty-terminal";
-                    "${basePath}/custom1" = customKeybinding "<Super>period" "code" "open-vsCode";
-                    "${basePath}/custom2" = customKeybinding "<Super>g" "goland" "open-goland-ide";
-                    "${basePath}/custom3" = customKeybinding "<Super>b" "chromium" "open-chromium-browser";
-                    "${basePath}/custom4" = customKeybinding "<Super>f" "nautilus" "open-files";
-                    "${basePath}/custom5" = customKeybinding "<Super><Shift>t" "alacritty" "open-allacrity-terminal";
+                    "${basePath}/custom1" = customKeybinding "<Super><Shift>t" "alacritty" "open-allacrity-terminal";
+                    "${basePath}/custom2" = customKeybinding "<Super>period" "code" "open-vsCode";
+                    "${basePath}/custom3" = customKeybinding "<Super>g" "goland" "open-goland-ide";
+                    "${basePath}/custom4" = customKeybinding "<Super>b" "chromium" "open-chromium-browser";
+                    "${basePath}/custom5" = customKeybinding "<Super><shift>b" "tor-browser" "open-tor-browser";
+                    "${basePath}/custom6" = customKeybinding "<Super>f" "nautilus" "open-files";
+                    "${basePath}/custom7" = customKeybinding "<Super>x" "telegram-desktop" "open-telegram-desktop";
                 };
             };
         };
