67170cca5a47fac8a4495c846e2760920f5579bb
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2025-04-22 07:13:25 +0700

    chore: move gui app to home

--- nixos/system/environment.nix
+++ nixos/system/environment.nix
@@ -5,25 +5,16 @@
         swww
         rofi-wayland
         dmidecode           # A tool that reads information about your system's hardware from the BIOS according to the SMBIOS/DMI standard
-        #jetbrains.goland
-        brave
         dart-sass           # dart sass for hugo
         brightnessctl       # brightness control
         hugo                # hugo
         gnumake             # for makefile
-        tor-browser         # tor browser
         doas                # alternative of sudo
         pgcli               # Rich command-line interface for PostgreSQL with auto-completion and syntax highlighting
         curl                # curl
         wget                # wget
-        pika-backup         # pika backup for backup data
-        # virtualbox          # virtual machine
+        networkmanagerapplet
         
-        # db clients
-        # dbeaver-bin         # multi clients database
-        # robo3t              # RoboMongo, mongodb client
-        # redisinsight      # redis insight, redis client
-
         # for go
         #go-tools
         #gopls
