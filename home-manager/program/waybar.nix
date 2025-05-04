b21e62e87673a4582c95b8d7586803d84c2285d7
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2025-05-05 03:22:34 +0700

    clarify: waybar conf

--- home-manager/program/waybar.nix
+++ home-manager/program/waybar.nix
@@ -9,50 +9,122 @@ in {
                 waybar = {
                     enable = true;
                     package = pkgs.waybar;
+                    systemd = {
+                        enable = false;
+                        target = "graphical-session.target";
+                    };
                     settings = {
                         mainBar = {
                             layer = "top";
                             position = "top";
-                            height = 30;
-                            output = [
-                                "eDP-1"
-                                "HDMI-A-1"
-                            ];
-                            modules-left = [ "wlr/taskbar" ];
-                            modules-center = [ "hyprland/workspaces" ];
-                            modules-right = [ "mpd" "custom/mymodule#with-css-id" "network" ];
+                            width = 1328;
+                            margin-top = 10;
                             
-                            "network" = {
-                                format = " ";
-                                format-disconnected = "睊 Disconnected";
-                                tooltip = true;
-                                on-click = "nm-connection-editor";
+                            # left
+                            modules-left = [ "custom/appLauncher" "clock" ];
+                            "custom/appLauncher" = {
+                                format = " ";
+                                tooltip = false;
+                                on-click = "rofi -show drun";
+                            };
+                            "clock" = {
+                                format = "{:%a, %d %b %Y | %H:%M}";
+                                interval = 1;
+                                locale = "id_ID.UTF-8";
+                            };
+                            # center
+                            modules-center = [ "hyprland/workspaces" ];
+                            "hyprland/workspaces" = {
+                                disable-scroll = true;
+                                all-outputs = true;
+                                on-click = "activate";
+                                persistent-workspaces = {
+                                    "1" = "[]";
+                                    "2" = "[]";
+                                    "3" = "[]";
+                                    "4" = "[]";
+                                    "5" = "[]";
+                                    "6" = "[]";
+                                    "7" = "[]";
+                                    "8" = "[]";
+                                    "9" = "[]";
+                                    "10" = "[]";
+                                };
                             };
 
-                            "hyprland/workspaces" = {
-                                disable-scroll = false;
-                                all-outputs = true;
-                                format = "{icon}";
+                            # right
+                            modules-right = [ "mpd" "pulseaudio" "network" "custom/powermenu" ];
+                            "mpd" = {
+                                format = "{artist}: {title}";
+                                on-click = "mpc toggle";
+                                interval = 2;
+                            };
+                            "pulseaudio" = {
+                                scroll-step = 1;
+                                format = "{icon} {volume}%";
+                                format-muted = "󰖁 Muted";
                                 format-icons = {
-                                    "1" = "";
-                                    "2" = "";
-                                    "3" = "";
-                                    "4" = "";
-                                    "5" = "";
-                                    "6" = "";
-                                    "7" = "";
-                                    "8" = "";
-                                    "9" = "";
-                                    "10" = "";
-                                    "active" = "";
-                                    "default" = "";
+                                    default = [ "" "" "" ];
                                 };
-                                persistent-workspaces = {
-                                    "*" = [ 1 2 3 4 5 6 7 8 9 10 ];
-                                };
+                                on-click = "pamixer -t";
+                                tooltip = false;
+                            };    
+                            "network" = {
+                                format = " ";
+                                format-ethernet = "  {ifname}";
+                                format-disconnected = "睊 Disconnected";
+                                tooltip-format = "📡 {ifname}\n🔌 SSID: {essid}\n📶 Strength: {signalStrength}%\n⬆️ Up: {bandwidthUpBits} \n⬇️ Down: {bandwidthDownBits}";
+                                interval = 5;
+                                on-click = "nm-connection-editor";
+                            };
+                            "custom/powermenu" = {
+                                format = "";
+                                on-click = "";
                             };
                         };
                     };
+                    style = ''
+                        #waybar {
+                            font-family: "JetBrainsMono Nerd Font";
+                            font-size: 12pt;
+                            font-weight: bold;
+                            border: 2px solid #dedede;
+                            border-radius: 9px; /* agar border terlihat penuh di semua sisi */
+                            background-color: rgba(0, 0, 0, 0.3); /* semi-transparent */
+                        }
+
+                        #custom-appLauncher {
+                            font-size: 20px;
+                            padding-left: 8px;
+                            padding-right: 6px;
+                            color: #7ebae4;
+                        }
+
+                        #clock, #mpd, #pulseaudio, #network {
+                            padding-left: 10px;
+                            padding-right: 10px;
+                            color: #dedede;
+                        }
+
+                        #custom-powermenu {
+                            padding-left: 10px;
+                            padding-right: 14px;
+                            color: #dedede;
+                        }
+
+                        #workspaces {
+                            padding: 0px;
+                            font-family: "JetBrainsMono Nerd Font";
+                            font-size: 12pt;
+                            font-weight: bold;
+                        }
+
+                        #workspaces button.active {
+                            background-color: #dedede;
+                            color: #ffffff;
+                            border-radius: 9px;
+                        }
+                    '';
                 };
             };
         };
