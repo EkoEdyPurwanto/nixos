c151cb0ed5859c30ab8cc30631882980ef9d10ca
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2025-08-11 12:14:37 +0700

    clarify: move tui package to env systm

--- home-manager/package/tui/tui.nix
+++ home-manager/package/tui/tui.nix
@@ -8,15 +8,13 @@ in {
             home = {
                 packages = with pkgs; [
                     ### TUI ###
-                    trashy          # if you do the command 'rm' or 'rm -rf' in the terminal it is automatically placed in the trash
                     wtf             # wtfutil for run
-                    nix-health      # check the health of your nix setup
-                    nix-search-cli  # cli for searching package
                     pipes           # pipes.sh for run
                     cmatrix         # matrix like hengker pro Tzy
                     cbonsai         # bonsai ASCII art
                     tty-clock       # to display the current time on terminal
                     # cointop       # cryptocurrency tracking like htop
+                    networkmanagerapplet
                 ];
             };
         };
